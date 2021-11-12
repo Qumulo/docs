edfa0025af77ad6602073d4500aebcfecb3e8b04
Michael G. Khmelnitsky <mkhmelnitsky@qumulo.com>
2021-11-12 15:50:10 -0800

    Regen PDF

--- pdf-administrator-guide.sh
+++ pdf-administrator-guide.sh
@@ -4,11 +4,11 @@ echo 'Kill all Jekyll instances'
 kill -9 $(ps aux | grep '[j]ekyll' | awk '{print $2}')
 clear
 
-echo "Building PDF-friendly HTML site for Mydoc ...";
-bundle exec jekyll serve --detach --config _config.yml,pdfconfigs/config_mydoc_pdf.yml;
+echo "Building PDF-friendly HTML site for Qumulo Administrator Guide...";
+bundle exec jekyll serve --detach --config _config.yml,pdfconfigs/config_administrator_guide_pdf.yml;
 echo "done";
 
-echo "Building the PDF ...";
-prince --javascript --input-list=_site/pdfconfigs/prince-list.txt -o pdf/qumulo-docs.pdf;
+echo "Building the PDF...";
+prince --javascript --input-list=_site/pdfconfigs/prince-list.txt -o pdf/qumulo-administrator-guide.pdf;
 
-echo "Done. Look in the pdf directory to see if it printed successfully."
+echo "Done. The PDF output is in the /pdf directory."
