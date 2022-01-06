4ae7b05d3ab95baa50c5f992b2c0174d136fdb92
Michael G. Khmelnitsky <mkhmelnitsky@qumulo.com>
2022-01-06 15:11:24 -0800

    Intermediate test.

--- _site/pdf-supermicro.sh
+++ _site/pdf-supermicro.sh
@@ -4,17 +4,14 @@ echo 'Kill all Jekyll instances'
 kill -9 $(ps aux | grep '[j]ekyll' | awk '{print $2}')
 clear
 
-echo ""
-echo "Building PDF-friendly HTML site for Qumulo on Azure as a Servide Onboarding Guide...";
-bundle exec jekyll serve --detach --config _config.yml,pdfconfigs/config_azure_pdf.yml;
+echo "Building PDF-friendly HTML site for Qumulo on Supermicro A+ WIO 1114S-WN10RT Getting Started Guide...";
+bundle exec jekyll serve --detach --config _config.yml,pdfconfigs/config_supermicro_pdf.yml;
 echo "done";
 
-echo ""
 echo "Building the PDF...";
-prince --javascript --input-list=_site/pdfconfigs/prince-list.txt -o pdf/qumulo-azure-getting-started-guide.pdf;
+prince --javascript --input-list=_site/pdfconfigs/prince-list.txt -o pdf/qumulo-supermicro-getting-started-guide.pdf;
 
-echo ""
 echo "Deleting temporary build files..."
-#cd _site && rm * -rf
+cd _site && rm * -rf
 
 echo "Done. The PDF output is in the /pdf directory."
