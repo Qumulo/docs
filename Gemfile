b9e5c1ca838cd1854cfd0ca07c8fcb0b5be3f1d1
Michael G. Khmelnitsky <mkhmelnitsky@qumulo.com>
2023-03-16 15:42:00 -0700

    Initial addition of html-proofer.

--- Gemfile
+++ Gemfile
@@ -6,4 +6,5 @@ gem 'github-pages', group: :jekyll_plugins
 # to publish without github page
 #gem "jekyll"
 
+gem 'nokogumbo'
 gem 'html-proofer'
--- docker/build/run.sh
+++ docker/build/run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 case "${1}" in
+    check)
+        echo "Checking HTML output";
+        htmlproofer _site ;;
     serve)
         echo "Serving on port 4000" ;
         jekyll serve --livereload -H 0.0.0.0 ;;
