5c81917ed727d885ebdadc46ec8454c79ef7446c
Michael G. Khmelnitsky <mkhmelnitsky@qumulo.com>
2023-03-22 13:41:31 -0700

    Added a message to run.sh

--- docker/build/run.sh
+++ docker/build/run.sh
@@ -2,13 +2,14 @@
 
 case "${1}" in
     clean)
-        rm _site -rf;;
+        echo "Cleaning up HTML output"
+        rm _site -rf ;;
     check)
         jekyll build -d _site
-        echo "Checking HTML output";
+        echo "Checking HTML output"
         htmlproofer _site ;;
     serve)
-        echo "Serving on port 4000" ;
+        echo "Serving on port 4000"
         jekyll serve --livereload -H 0.0.0.0 ;;
     *)
         jekyll build -d _site ;;
