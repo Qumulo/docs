fab372c7850c18817a03720969663e0600a61313
Michael G. Khmelnitsky, Ph.D <mig281@users.noreply.github.com>
2023-03-30 17:29:58 -0700

    Update pdf-build.sh

--- pdf-build.sh
+++ pdf-build.sh
@@ -10,53 +10,89 @@ if [ $(docker ps | grep -c 'docs-container') -gt 0 ]; then
   docker kill docs-container
 fi
 
+# Declaring a future array of actions
+# Actions are what the script will do
+ACTIONS=()
+
+# Options are what the user will choose
 PS3='Choose a guide to build:'
-options=("Hardware Guide" "Azure Guide" "Administrator Guide" "Qumulo Alerts Guide")
+
+# Prints the options on screen
+options=("Hardware Guide" "Azure Guide" "Administrator Guide" "Qumulo Alerts Guide" "All Guides")
+
 select opt in "${options[@]}"
+
+# Matches the option to the action
+# Populates the ACTIONS array
 do
   case $opt in
     "Hardware Guide")
+      ACTIONS+=("Hardware Guide")
+      ;;
+    "Azure Guide")
+      ACTIONS+=("Azure Guide")
+      ;;
+    "Administrator Guide")
+      ACTIONS+=("Administrator Guide")
+      ;;
+    "Qumulo Alerts Guide")
+      ACTIONS+=("Qumulo Alerts Guide")
+      ;;
+    "All Guides")
+      # Perform all of the actions
+      ACTIONS+=("Hardware Guide" "Azure Guide" "Administrator Guide" "Qumulo Alerts Guide" )      
+      ;;      
+    *) echo "$REPLY is not valid. Try again."
+    ;;
+  esac
+done
+
+# Define the build config and output filename for each guide
+function build_prince {
+  # Match input against the first parameter
+  case "$1" in
+    "Hardware Guide")
       JEKYLL_CONFIG=config_hardware_guide_pdf.yml
       PRINCE_OUTPUT=qumulo-certified-hardware-guide.pdf
-      break
       ;;
     "Azure Guide")
       JEKYLL_CONFIG=config_azure_guide_pdf.yml
       PRINCE_OUTPUT=qumulo-azure-guide.pdf
-      break
       ;;
     "Administrator Guide")
       JEKYLL_CONFIG=config_administrator_guide_pdf.yml
       PRINCE_OUTPUT=qumulo-administrator-guide.pdf
-      break
       ;;
     "Qumulo Alerts Guide")
       JEKYLL_CONFIG=config_qumulo_alerts_guide_pdf.yml
       PRINCE_OUTPUT=qumulo-alerts-guide.pdf
-      break
       ;;
-    *) echo "$REPLY is not valid. Try again.";;
   esac
-done
+
+  echo "Building the PDF..."
+  prince --javascript --input-list=_site/pdfconfigs/prince-list.txt -o "pdf/${PRINCE_OUTPUT}"
+}
 
 echo "Building PDF-friendly HTML site..."
 docker run -ti \
-        --rm \
-        --user $(id -u):$(id -g) \
-        --name docs-container \
-        -v "$(pwd)":/src:rw \
-        -P \
-        --detach \
-        --network host docs-builder serve \
-        --config "_config.yml,pdfconfigs/${JEKYLL_CONFIG}"
+  --rm \
+  --user $(id -u):$(id -g) \
+  --name docs-container \
+  -v "$(pwd)":/src:rw \
+  -P \
+  --detach \
+  --network host docs-builder serve \
+  --config "_config.yml,pdfconfigs/${JEKYLL_CONFIG}"
 
 echo "Waiting for port 4000 to become available..."
 while ! nc -z localhost 4000; do
   sleep 0.2
 done
 
-echo "Building the PDF..."
-prince --javascript --input-list=_site/pdfconfigs/prince-list.txt -o "pdf/${PRINCE_OUTPUT}"
+# Build the PDF for each item in the array
+for i in ${ACTIONS[@]}: do
+  build_prince "$i"
+done
 
 echo "Deleting temporary build files..."
 cd _site && rm * -rf
