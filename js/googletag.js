3d844d5a8add80eaae0d12644a86f4b38b46acc0
Michael G. Khmelnitsky, Ph.D <mig281@users.noreply.github.com>
2024-04-22 12:18:44 -0700

    Updating EEA consent mode to v2

--- js/googletag.js
+++ js/googletag.js
@@ -5,5 +5,7 @@ gtag('config', 'G-81J7N4LMSE', {cookie_flags: 'SameSite=None;Secure'});
 gtag('consent', 'default', {
   'ad_storage': 'denied',
   'analytics_storage': 'denied',
+  'ad_user_data': 'denied',
+  'ad_personalization': 'denied',  
   'region': ['AT', 'BE', 'BG', 'HR', 'CY', 'CZ', 'DK', 'EE', 'FI', 'FR', 'DE', 'GR', 'HU', 'IS', 'IE', 'IT', 'LV', 'LI', 'LT', 'LU', 'MT', 'NL', 'NO', 'PL', 'PT', 'RO', 'SK', 'SI', 'ES', 'SE', 'CH', 'UK']
 });
