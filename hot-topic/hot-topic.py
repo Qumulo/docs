e4445618626d0869ef44ccd4557bd32bad6161df
Lucía M. Polis <lucia@qumulo.com>
2025-07-30 16:40:02 -0700

    DOCS-2944

--- hot-topic/hot-topic.py
+++ hot-topic/hot-topic.py
@@ -54,7 +54,10 @@ def generate_markdown(filtered_rows):
     for idx, row in enumerate(filtered_rows[:10], start=1):
         dimensions = row.dimension_values
         page_path = dimensions[0].value
-        page_title = dimensions[1].value.replace(" | Qumulo Documentation Portal", "")
+        if dimensions[1].value == "Qumulo Documentation Portal":
+            page_title = "Qumulo Documentation Portal Home"
+        else:
+            page_title = dimensions[1].value.replace(" | Qumulo Documentation Portal", "")
         markdown_lines.append("{}. [{}]({})".format(idx, page_title, page_path))
 
     markdown_lines.append("\n")  # Add a blank line after the last item
