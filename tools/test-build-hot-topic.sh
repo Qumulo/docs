78f58944aab0c3b690cacc2089ca35c4d2391c55
Lucía M. Polis <lucia@qumulo.com>
2025-10-17 16:04:35 -0700

    Allow the script to run locally OR in GH Actions

--- tools/test-build-hot-topic.sh
+++ tools/test-build-hot-topic.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # If running on GitHub Actions, get secret from GitHub
-[ -n "${ENGINEERING_JSON:-}" ]] && echo -n "$ENGINEERING_JSON" | base64 --decode > tools/hot-topic/engineering-173019-f79d4f9c2e03.json
+[[ -n "${ENGINEERING_JSON:-}" ]] && echo -n "$ENGINEERING_JSON" | base64 --decode > tools/hot-topic/engineering-173019-f79d4f9c2e03.json
 
 # Rebuild the google-analytics-script container
 docker build --file tools/hot-topic/Dockerfile --tag google-analytics-script ./tools/hot-topic
