759c505aab69039e0031ecef7d8850091d07d064
Lucía M. Polis <lucia@qumulo.com>
2025-10-17 15:01:46 -0700

    Pointing to correct credentials.

--- tools/test-build-hot-topic.sh
+++ tools/test-build-hot-topic.sh
@@ -2,13 +2,13 @@
 set -euo pipefail
 
 # Get secret from GitHub
-echo -n "$ENGINEERING_JSON" | base64 --decode > tools/hot-topic/engineering-173019-49cb92815171.json
+echo -n "$ENGINEERING_JSON" | base64 --decode > tools/hot-topic/engineering-173019-419af4b8908c.json
 
 # Rebuild the google-analytics-script container
 docker build --file tools/hot-topic/Dockerfile --tag google-analytics-script ./tools/hot-topic
 
 # Run the google-analytics-script image to regenerate part of README.md on the mainline branch
-docker run --volume $(pwd)/tools/hot-topic/engineering-173019-49cb92815171.json:/app/engineering-173019-49cb92815171.json --volume $(pwd)/README.md:/app/README.md google-analytics-script
+docker run --volume $(pwd)/tools/hot-topic/engineering-173019-419af4b8908c.json:/app/engineering-173019-419af4b8908c.json --volume $(pwd)/README.md:/app/README.md google-analytics-script
 
 # Remove key
 rm -f tools/hot-topic/*.json
