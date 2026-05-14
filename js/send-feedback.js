18af7cd59c43f5d80b05c6664545468dcd31b52c
Team Qontent <qontent@qumulo.com>
2026-05-14 23:58:34 +0000

    Rebuilt documentation website

--- js/send-feedback.js
+++ js/send-feedback.js
@@ -2,12 +2,18 @@ document.getElementById("send-feedback").addEventListener("click", () => {
   const width = 575;
   const height = 755;
 
-  // center on the current screen
-  const left = Math.max(0, (window.screen.width  - width)  / 2);
-  const top  = Math.max(0, (window.screen.height - height) / 2);
+  const left = Math.max(0, (window.screen.width - width) / 2);
+  const top = Math.max(0, (window.screen.height - height) / 2);
+
+  const hardwareForm = "https://qumulo.atlassian.net/jira/software/form/07aa529f-ac1d-4d92-8da9-8cc1315323d9";
+  const defaultForm = "https://qumulo.atlassian.net/jira/software/form/a3eaa618-84a6-47ec-8a9e-5a0c3b016bc5";
+
+  const formUrl = window.location.href.includes("/hardware-guide") 
+    ? hardwareForm 
+    : defaultForm;
 
   window.open(
-    "https://qumulo.atlassian.net/jira/software/form/a3eaa618-84a6-47ec-8a9e-5a0c3b016bc5",
+    formUrl,
     "jiraForm",
     `popup=yes,width=${width},height=${height},left=${left},top=${top},noopener`
   );
